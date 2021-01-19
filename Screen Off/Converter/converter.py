ac421be71fa838f6e2ba697789c9107d2220072e
Eduardo Lima <39864628+emarquesdelima@users.noreply.github.com>
2021-01-19 15:18:29 -0300

    Ajustes do fluxo da fórmula

--- Screen Off/Converter/converter.py
+++ Screen Off/Converter/converter.py
@@ -1,25 +1,14 @@
-
-
 class Converter:
-
-    def volume_cilindrto_litros(self, raio, altura):
-
+    def volume_cilindrto_litros(self):
+        raio = float(input("Qual o raio do cilindro em metros? "))
+        altura = float(input("Qual a altura do cilindro em metros? "))        
         pi = 3.14
-
         print(f"O raio é: {raio}")
-        print(f"A altura é: {altura}")
-        
+        print(f"A altura é: {altura}")        
         volume = pi * (raio**2) * altura
-
         volume_litros = volume * 1000
-
         print(f"O volume em litros é: {volume_litros}")
-
         pass
 
 calc = Converter()
-
-raio = float(input("Qual o raio do cilindro em metros? "))
-altura = float(input("Qual a altura do cilindro em metros? "))
-
-calc.volume_cilindrto_litros(raio,altura)
+calc.volume_cilindrto_litros()
